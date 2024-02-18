285984df3c1f0000a93bba5285281180768cb3a9
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2024-02-18 16:54:34 +0530

    Update EIPBoto3.py

--- Eip/EIPBoto3.py
+++ Eip/EIPBoto3.py
@@ -1,12 +1,18 @@
 import boto3
-client = boto3.client('ec2')
-addresses = client.describe_addresses()['Addresses']
-for eip in addresses:
-    if ('AssociationId' not in eip) and ('NetworkInterfaceId' not in eip):
-        print(eip['PublicIp'] + " is unused, releasing")
-        client.release_address(AllocationId=eip['AllocationId'])
-    else:
-        print(eip['PublicIp'] + "is used, not releasing")    
+def lambda_handler(event, context):
+    client = boto3.client('ec2')
+    addresses = client.describe_addresses()['Addresses']
+    for eip in addresses:
+        if ('AssociationId' not in eip) and ('NetworkInterfaceId' not in eip):
+            print(eip['PublicIp'] + " is unused, releasing")
+            client.release_address(AllocationId=eip['AllocationId'])
+        else:
+            print(eip['PublicIp'] + "is used, not releasing")
+    return {
+        'statusCode': 200,
+        'body': 'Unused Elastic IPs deleted successfully!'
+    }
+    
     
     
 
